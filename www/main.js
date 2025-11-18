7080db3c84501af436f24b3bfab593396ebabf59
your Name <ramees@RameesRoshan>
2025-11-18 23:15:05 +0530

    hood design done

---
+++ www/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bouncein",
+        },
+        out: {
+            effect: "bounceout",
+        },
+    })
+
+});         
